a017179fba6c55e4a6e7e994c85d40041c335159
Mayur Pawar <mayur.pawar934@gmail.com>
2020-10-20 16:21:15 +0530

    added notations

--- myCalculator.py
+++ myCalculator.py
@@ -1,19 +1,22 @@
 from collections import OrderedDict
+
+# function lib
 from Utils import *
 
 
 class TheCalculator:
 
     def __init__(self):
+        # all mathematical operations
         self.operations = OrderedDict([
             ("+", lambda x, y: add(x, y)),
             ("-", lambda x, y: sub(x, y)),
             ("*", lambda x, y: mul(x, y)),
             ("/", lambda x, y: div(x, y)),
             ("//", lambda x, y: floor_div(x, y)),
-            ("&", lambda x, y: mod(x, y)),
-            ("^", lambda x, y: square(x, y)),
-            ("$", lambda x, y: sqr_root(x)),
+            ("%", lambda x, y: mod(x, y)),
+            ("^", lambda x, y: power(x, y)),
+            ("&", lambda x, y: sqr_root(x)),
         ])
 
     def expressions(self, var):
@@ -47,3 +50,28 @@ class TheCalculator:
 
     def calc(self, var):
         return self.evaluate(self.expressions(var))
+
+
+if __name__ == '__main__':
+
+    instructions = """<<<    My Calculator    >>>
+README:
+    Addition (+)
+    Subtraction (-)
+    Multiplication (*)
+    Division (/)
+    Power (^)
+    Square-root (&)
+    mod (%)
+    eg. multiply 15*6
+    eg. mod 10%2
+    """
+    print(instructions)
+
+    exe = TheCalculator()
+    try:
+        while True:
+            ans = exe.calc(input("Calculator:"))
+            print("ans= : ", ans)
+    except KeyboardInterrupt:
+        print("\n\n Thank You.! Have a Good Day")
