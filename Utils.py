848b5ee9207599c06af83866c9e469e1c38b2e7d
Mayur Pawar <mayur.pawar934@gmail.com>
2020-10-20 14:26:19 +0530

    initial commit

---
+++ Utils.py
@@ -0,0 +1,30 @@
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
+
+
+def floor_div(x, y):
+    return x // y
+
+
+def mod(x, y):
+    return x % y
+
+
+def square(x, y):
+    return x ** y
+
+
+def sqr_root(x, y=0.5):
+    return x**y
